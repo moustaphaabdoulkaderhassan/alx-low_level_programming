7510347a02df9fb54e6e7ebdf1aa66b4396c7fff
moustaphaabdoulkaderhassan <moustaphaabdoulkader341@outlook.com>
2023-10-27 09:31:39 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,22 +3,18 @@
  * _puts_recursion - prints a string
  *
  * @s: string to print
- *    
- * Return: nothing
+ *
+ *Return: void
  */
+
+
 void _puts_recursion(char *s)
 {
-		if (*s == '\0') 
-		 {    
-		      
-		           _putchar(*s);
-		           _puts_recursion(s + 1);
-	         }  
+		if (*s == '\0')
 				{
-		if (*s == '\0') 					
-			
-	       	{ 
-			_putchar('\n');
-
-		}
+							_putchar('\n');
+									return;
+										}
+			_putchar(*s);
+				_puts_recursion(s + 1);
 }
