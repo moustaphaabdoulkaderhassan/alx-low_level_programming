2a0e3cd85ae8d2228a0ead050bf6a9b34664c8c5
moustaphaabdoulkaderhassan <moustaphaabdoulkader341@outlook.com>
2023-11-09 05:56:03 -0800

    3-main.c

--- 0x10-variadic_functions/3-main.c
+++ 0x10-variadic_functions/3-main.c
@@ -1,13 +1,12 @@
 #include "variadic_functions.h"
 
 /**
- * main - check the code for ALX School students.
+ * main - check the code
  *
  * Return: Always 0.
-*/
+ */
 int main(void)
 {
-	print_all("ceis", 'H', 0, "lberton");
-	print_all("fcsi", 3.14435, 'H', NULL, 402);
-	return (0);
+    print_all("ceis", 'B', 3, "stSchool");
+    return (0);
 }
