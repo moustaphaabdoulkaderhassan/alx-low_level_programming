b32c4e19564fbe7e5213d5aa8e004a99d1f80af1
moustaphaabdoulkaderhassan <moustaphaabdoulkader341@outlook.com>
2023-11-10 07:24:33 -0800

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,9 +1,7 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
 /**
- * sum_them_all - Entry Point
- * @n: variadic inputs
+ * sum_them_all - sum all parameter passed to the function 
+ * @n: number of argument to be passed to sum
  * Return: sum
  */
 int sum_them_all(const unsigned int n, ...)
